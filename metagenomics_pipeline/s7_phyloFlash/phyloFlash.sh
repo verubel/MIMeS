9105189eb44ac6a7828f753b8689a0f5760d86cf
verubel <118374329+verubel@users.noreply.github.com>
2023-06-21 13:59:05 +0200

    Create phyloFlash.sh
    added command line for phyloFlash

---
+++ metagenomics_pipeline/s7_phyloFlash/phyloFlash.sh
@@ -0,0 +1,6 @@
+## phyloFlash https://github.com/HRGV/phyloFlash can not be included into bioconda on the ELWE cluster because of version conflics
+
+rhrk-singularity phyloflash_3.4.2--hdfd78af_1.simg "phyloFlash.pl -lib output_name -dbhome /dir/to/db/138.1 \
+ -read1 /dir/to/file_R1.fastq.gz \
+ -read2 /dir/to/file_R2.fastq.gz \
+ -almosteverything"
