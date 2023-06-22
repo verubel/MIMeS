3499f707b24b581202a7133ca826fa9b73eb306b
verubel <118374329+verubel@users.noreply.github.com>
2023-06-22 16:08:57 +0200

    Update megahit_coassembly.sh
    updated megahit script to include paried end processing

--- metagenomics_pipeline/s6_MEGAHIT/megahit_coassembly.sh
+++ metagenomics_pipeline/s6_MEGAHIT/megahit_coassembly.sh
@@ -13,18 +13,17 @@ module load bioconda/latest
 # Run MEGAHIT co-assembly
 # -m 0.90 allows memomry allocation of 90%
 # --presets meta-large sets kmers k list: 27,37,47,57,67,77,87,97,107,117,127 
+# example: coassembly of 5 samples, paired end (forward=R1, reverse=R2)
 
 megahit \
-  -1 /G1_fastq/R1_paired_5462_K.fastq \
-  -1 /G1_fastq/R1_paired_5707_H.fastq \
-  -1 /G1_fastq/R1_paired_5462_M.fastq \
-  -1 /G1_fastq/R1_paired_5707_I.fastq \
-  -1 /G1_fastq/R1_paired_5707_F.fastq \
-  -1 /G1_fastq/R1_paired_5707_G.fastq \
-  -2 /G1_fastq/R2_paired_5462_K.fastq \
-  -2 /G1_fastq/R2_paired_5707_H.fastq \
-  -2 /G1_fastq/R2_paired_5462_M.fastq \
-  -2 /G1_fastq/R2_paired_5707_I.fastq \
-  -2 /G1_fastq/R2_paired_5707_F.fastq \
-  -2 /G1_fastq/R2_paired_5707_G.fastq \
+  -1 /dir/to/file1_R1.fastq \
+  -1 /dir/to/file2_R1.fastq \
+  -1 /dir/to/file3_R1.fastq \
+  -1 /dir/to/file4_R1.fastq \
+  -1 /dir/to/file5_R1.fastq \
+  -2 /dir/to/file1_R2.fastq \
+  -2 /dir/to/file2_R2.fastq \
+  -2 /dir/to/file3_R2.fastq \
+  -2 /dir/to/file4_R2.fastq \
+  -2 /dir/to/file5_R2.fastq \
   -m 0.9 -t 32 --presets meta-large -o G1_megahit_out
