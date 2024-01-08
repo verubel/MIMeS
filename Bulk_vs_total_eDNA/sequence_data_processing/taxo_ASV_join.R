5b046e6fd359392463072d46cd01a43955d56cd3
verubel <118374329+verubel@users.noreply.github.com>
2024-01-08 15:02:22 +0100

    Update taxo_ASV_join.R

--- Bulk_vs_total_eDNA/sequence_data_processing/taxo_ASV_join.R
+++ Bulk_vs_total_eDNA/sequence_data_processing/taxo_ASV_join.R
@@ -17,7 +17,7 @@ table(both$kingdom)
 # both %>% 
 #   group_by(domain) %>%
 #   summarise(no_rows = length(domain))
-target_asvs <- both %>% filter(kingdom=="Bacteria") %>% droplevels()
+target_asvs <- both %>% filter(kingdom=="Metazoa") %>% droplevels()
 asv_tab_taxo <- data.frame(target_asvs, stringsAsFactors = F)
 asv_tab_taxo_no_singletons <- asv_tab_taxo[which(rowSums(asv_tab_taxo[,2:13])>1),]
 write.csv2(asv_tab_taxo_no_singletons, "ASV_Table_Taxo.csv")
