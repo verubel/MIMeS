5251f346bea2bed2568ceeb50810638992402787
verubel <118374329+verubel@users.noreply.github.com>
2024-01-31 10:09:07 +0100

    Update clumpify.sh
    changed format to .sh

--- metagenomics_pipeline/clumpify/clumpify.sh
+++ metagenomics_pipeline/clumpify/clumpify.sh
@@ -1,14 +1,21 @@
-# already implemented: bbmap
-# tool for the removal of duplicates
-# also able to remove optical duplicates
+#!/bin/sh
+#SBATCH -N 1
+#SBATCH --account=RPTU-MIMeS
+#SBATCH --tasks=24
+#SBATCH --mem=90000
+#SBATCH --time=01:30:00
 
-for i in `cat /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/samples.txt`
-do
-  echo ${i}
-  SAMPLE=$(echo ${i})
-  mkdir ${SAMPLE}
+SAMPLE_LIST="/work/RPTU-MIMeS/concat_rawfiles_SO295/samples.txt"
+
+# Loop through each sample in the list
+for SAMPLE in $(cat "$SAMPLE_LIST"); do
+  # Create a directory for each sample
+  mkdir "${SAMPLE}"
+  # Load the required modules
   module load bioconda/latest
-  sbatch -N 1 --account=RPTU-MIMeS --tasks=24 --mem=100000 --time=06:00:00 --wrap="clumpify.sh in=/work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step2_bbduk/${SAMPLE}/${SAMPLE}_cleaned_R1.fastq.gz \
-  in2=/work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step2_bbduk/${SAMPLE}/${SAMPLE}_cleaned_R1.fastq.gz out=${SAMPLE}/${SAMPLE}_dedup_R1.fastq.gz out2=${SAMPLE}/${SAMPLE}_dedup_R2.fastq.gz \
-  dedupe optical dist=40"
+  # Submit the sbatch job for each sample
+  clumpify.sh in=/work/RPTU-MIMeS/DNA_processing/bbduk/${SAMPLE}/${SAMPLE}_cleaned_R1.fastq.gz \
+  in2=/work/RPTU-MIMeS/DNA_processing/bbduk/${SAMPLE}/${SAMPLE}_cleaned_R2.fastq.gz out=${SAMPLE}/${SAMPLE}_dedup_R1.fastq.gz out2=${SAMPLE}/${SAMPLE}_dedup_R2.fastq.gz \
+  dedupe optical dist=40
 done
+
