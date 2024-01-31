0ba5c8e609404122f7f2fd49fc6e11e797f68c53
verubel <118374329+verubel@users.noreply.github.com>
2024-01-31 09:28:03 +0100

    Update bbduk_trimming.sh
    added .sh script

--- metagenomics_pipeline/bbduk/bbduk_trimming.sh
+++ metagenomics_pipeline/bbduk/bbduk_trimming.sh
@@ -1,9 +1,20 @@
-for i in `cat /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/samples.txt`
-do
-  SAMPLE=$(echo ${i})
-  mkdir ${SAMPLE}
+#!/bin/sh
+#SBATCH -N 1
+#SBATCH --account=RPTU-MIMeS
+#SBATCH --tasks=12
+#SBATCH --mem=10000
+#SBATCH --time=01:00:00
+
+SAMPLE_LIST="/work/RPTU-MIMeS/concat_rawfiles_SO295/samples.txt"
+
+# Loop through each sample in the list
+for SAMPLE in $(cat "$SAMPLE_LIST"); do
+  # Create a directory for each sample
+  mkdir "${SAMPLE}"
+  # Load the required modules
   module load bioconda/latest
-  sbatch -N 1 --account=RPTU-MIMeS --tasks=24 --mem=100000 --time=06:00:00 --wrap="bbduk.sh in1=/work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/raw_test_data/${SAMPLE}_R1.fastq.gz \
-  in2=/work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/raw_test_data/${SAMPLE}_R2.fastq.gz out1=${SAMPLE}/${SAMPLE}_cleaned_R1.fastq.gz out2=${SAMPLE}/${SAMPLE}_cleaned_R2.fastq.gz \
-  threads=24 qtrim=rl trimq=30 minlength=100"
+  # Submit the sbatch job for each sample
+  bbduk.sh in1=/work/RPTU-MIMeS/concat_rawfiles_SO295/${SAMPLE}_R1.fastq.gz \
+  in2=/work/RPTU-MIMeS/concat_rawfiles_SO295/${SAMPLE}_R2.fastq.gz out1=${SAMPLE}/${SAMPLE}_cleaned_R1.fastq.gz out2=${SAMPLE}/${SAMPLE}_cleaned_R2.fastq.gz \
+  threads=12 qtrim=rl trimq=30 minlength=100
 done
