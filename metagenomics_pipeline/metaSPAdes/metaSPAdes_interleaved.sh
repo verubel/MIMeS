b1443911028cc5ed1fcdee98a5a250c1ee3cb2a2
verubel <118374329+verubel@users.noreply.github.com>
2024-02-06 13:43:12 +0100

    Update metaSPAdes_interleaved.sh
    make sh formatted

--- metagenomics_pipeline/metaSPAdes/metaSPAdes_interleaved.sh
+++ metagenomics_pipeline/metaSPAdes/metaSPAdes_interleaved.sh
@@ -1,8 +1,19 @@
+#!/bin/sh
+#SBATCH -N 1
+#SBATCH --account=RPTU-MIMeS
+#SBATCH --tasks=32
+#SBATCH --mem=900000
+#SBATCH --time=48:00:00
+
 # interleaved mode
-module load bioconda/latest
-for i in `cat /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/samples.txt`
-do
-  echo ${i}
-  SAMPLE=$(echo ${i})
-  sbatch -N 1 --account=RPTU-MIMeS --tasks=32 --mem=900000 --time=48:00:00 --mail-type=END --wrap "spades.py --meta --12 /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step3_sortmerna/${SAMPLE}/sortme_notaligned_${SAMPLE}.fq.gz -t 32 -m 900 --phred-offset 33 -o /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step4a_metaspades_INTERLEAVED/${SAMPLE}"
+SAMPLE_LIST="/work/RPTU-MIMeS/concat_rawfiles_SO295/samples.txt"
+
+# Loop through each sample in the list
+for SAMPLE in $(cat "$SAMPLE_LIST"); do
+  # Load the required modules
+  module load bioconda/latest
+  # Submit the sbatch job for each sample
+  # metaspades
+  spades.py --meta --12 /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step3_sortmerna/${SAMPLE}/sortme_notaligned_${SAMPLE}.fq.gz \
+  -t 32 -m 900 --phred-offset 33 -o /work/RPTU-MIMeS/pipeline_pilot_DNA/first_steps/step4a_metaspades_INTERLEAVED/${SAMPLE}
 done
